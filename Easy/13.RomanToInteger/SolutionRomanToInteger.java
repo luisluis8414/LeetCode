ee44c7d601a8a6733711ab247d1fe981e59a0918
luisluis8414 <luis.wehrberger2@gmail.com>
2023-02-24 10:53:52 +0100

    first commit

---
+++ Easy/13.RomanToInteger/SolutionRomanToInteger.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int romanToInt(String s) {
+        int num=0;
+        char[] roman = s.toCharArray();
+        for(int i=0;i<roman.length;i++)
+        {
+            if(roman[i]=='I')
+            {   
+                num+=1;
+                if(i!=roman.length-1){
+                if(roman[i+1]=='V'||roman[i+1]=='X')
+                {
+                 num-=2;
+                }
+                }
+            
+                
+            }
+            if(roman[i]=='V')
+            {
+                num+=5;
+            }
+            if(roman[i]=='X')
+            {
+                num+=10;
+                if(i!=roman.length-1)
+                {
+                if(roman[i+1]=='L'||roman[i+1]=='C')
+                    {
+                    num-=20;
+                    }
+                
+                }
+            }
+            if(roman[i]=='L')
+            {
+                num+=50;
+            }
+            if(roman[i]=='C')
+            {
+                num+=100;
+                if(i!=roman.length-1)
+                {
+                    if(roman[i+1]=='D'||roman[i+1]=='M')
+                    {
+                        num-=200;
+                    }
+                }
+            }
+            if(roman[i]=='D')
+            {
+                num+=500;
+            }
+            
+            if(roman[i]=='M')
+            {
+                num+=1000;
+            }
+        }
+        
+        return num; 
+    }
+}
