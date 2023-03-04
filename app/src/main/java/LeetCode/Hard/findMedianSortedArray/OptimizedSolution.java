2f7118520a25d395a53254d62edbbe5615eec089
luisluis8414 <luis.wehrberger2@gmail.com>
2023-03-04 15:43:21 +0100

    Solution and Optimized Solition

---
+++ app/src/main/java/LeetCode/Hard/findMedianSortedArray/OptimizedSolution.java
@@ -0,0 +1,31 @@
+package LeetCode.Hard.findMedianSortedArray;
+
+public class OptimizedSolution {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        int l1=nums1.length;
+        int l2=nums2.length;
+        int l3=l1+l2;
+        int[] mergedArr=new int[l3];//4
+
+        int i=0;
+        int j=0;
+        int t=0;
+
+        while (i < l1 || j < l2) {
+            if (i < l1 && j < l2) {
+                mergedArr[t++] = (nums1[i] < nums2[j]) ? nums1[i++] : nums2[j++];
+            } else if (i < l1) {
+                mergedArr[t++] = nums1[i++];
+            } else {
+                mergedArr[t++] = nums2[j++];
+            }
+        }
+
+        if(l3%2!=0){
+           return (double)mergedArr[l3/2];
+        }else{
+            return (((double)mergedArr[(l3/2)-1])+((double)mergedArr[l3/2]))/2;
+        }
+    }
+}
---
+++ app/src/main/java/LeetCode/Hard/findMedianSortedArray/Solution.java
@@ -0,0 +1,47 @@
+package LeetCode.Hard.findMedianSortedArray;
+
+public class Solution {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        int l1=nums1.length;//2
+        int l2=nums2.length;//2
+        int[] mergedArr=new int[l1+l2];//4
+
+        int i=0;
+        int j=0;
+        int t=0;
+
+        while(i<l1&&j<l2)
+        {
+            if(nums1[i]<nums2[j]){
+                mergedArr[t]=nums1[i];
+                i++;
+            }else{
+                mergedArr[t]=nums2[j];
+                j++;
+            }
+            t++;
+        }
+        while(i<l1||j<l2){
+            if(i<l1){
+                mergedArr[t]=nums1[i];
+                i++;
+                t++;
+            }
+            if(j<l2){
+                mergedArr[t]=nums2[j];
+                j++;
+                t++;
+            }
+        }
+        
+
+        if(mergedArr.length%2!=0){
+           return (double)mergedArr[mergedArr.length/2];
+        }else{
+            double index1=(double)mergedArr[(mergedArr.length/2)-1];
+            double index2=(double)mergedArr[mergedArr.length/2];
+            return (index1+index2)/2;
+        }
+    }
+}
